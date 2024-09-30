8cc7382b3ee565c67fcd0403d9e5f51d79047b39
HarshSaini3878 <142294106+HarshSaini3878@users.noreply.github.com>
2024-09-30 23:27:17 +0530

    step 3

--- index.js
+++ index.js
@@ -2,6 +2,8 @@ const express=require(express);
 const app=express();
 const {userRouter}=require("./users");
 const {courseRouter}=require("./course");
+const {adminRouter}=require("./admin");
 app.use("api/v1/user",userRouter);
 app.use("api/v1/course",courseRouter);
+app.use("api/v1/admin",adminRouter);
 app.listen(3000);
