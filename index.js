db052f2ad2daa844ac3b93e29d650f3d5b0627e8
HarshSaini3878 <142294106+HarshSaini3878@users.noreply.github.com>
2024-09-30 23:07:52 +0530

    step 2

--- index.js
+++ index.js
@@ -1,1 +1,5 @@
-const express
+const express=require(express);
+const app=express();
+const {userRouter}=require("./users");
+app.use("api/v1/user",userRouter);
+app.listen(3000);
---
+++ users.js
@@ -0,0 +1,8 @@
+const {Router}=require("express");
+const userRouter =Router();
+userRouter.post('/signin',(req,res)=>{
+
+})
+module.exports={
+    userRouter:userRouter
+}
