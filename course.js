f4c17f91741bcf5a2e93b59a42867f539adff761
HarshSaini3878 <142294106+HarshSaini3878@users.noreply.github.com>
2024-09-30 23:10:19 +0530

    step 3

---
+++ course.js
@@ -0,0 +1,8 @@
+const {Router}=require("express");
+const courseRouter =Router();
+courseRouter.post('/signin',(req,res)=>{
+
+})
+module.exports={
+    courseRouter:courseRouter
+}
--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 const express=require(express);
 const app=express();
 const {userRouter}=require("./users");
+const {courseRouter}=require("./course");
 app.use("api/v1/user",userRouter);
+app.use("api/v1/course",courseRouter);
 app.listen(3000);
